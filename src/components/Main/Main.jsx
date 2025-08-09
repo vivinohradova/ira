5f21e0d9712ca787ab6ebb26831cdd1d13af06dc
vivinohradova2 <vynohradova.frontend@gmail.com>
2025-08-09 18:16:38 +0300

    fix gamburger popup

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import scrollToElement from "../ScrollUtils/ScrollUtils";
 import { GiHamburgerMenu } from "react-icons/gi";
 import WhatsAppButton from "../WhatsAppButton/WhatsAppButton";
+import PaymentButton from "../PaymentButton/PaymentButton";
 
 const Main = () => {
   const { t } = useTranslation();
@@ -29,37 +30,41 @@ const Main = () => {
     <>
       <main id="main" className={styles.popupOpen}>
         <div className={styles.container}>
-          <nav className={styles.menu}>
-            <li>
-              <a href="#services" onClick={(e) => handleClick(e, "services")}>
-                <button type="button">{t("menuService")}</button>
-              </a>
-            </li>
+          <div className={styles.menuWrapp}>
+            <nav className={styles.menu}>
+              <li>
+                <a href="#services" onClick={(e) => handleClick(e, "services")}>
+                  <button type="button">{t("menuService")}</button>
+                </a>
+              </li>
 
-            <li>
-              <a href="#about" onClick={(e) => handleClick(e, "about")}>
-                <button type="button">{t("menuAboutMe")}</button>
-              </a>
-            </li>
+              <li>
+                <a href="#about" onClick={(e) => handleClick(e, "about")}>
+                  <button type="button">{t("menuAboutMe")}</button>
+                </a>
+              </li>
 
-            <li>
-              <a href="#reviews" onClick={(e) => handleClick(e, "reviews")}>
-                <button type="button">{t("menuReviews")}</button>
-              </a>
-            </li>
+              <li>
+                <a href="#reviews" onClick={(e) => handleClick(e, "reviews")}>
+                  <button type="button">{t("menuReviews")}</button>
+                </a>
+              </li>
 
-            <li>
-              <a href="#contacts" onClick={(e) => handleClick(e, "contacts")}>
-                <button type="button">{t("menuContact")}</button>
-              </a>
-            </li>
-          </nav>
+              <li>
+                <a href="#contacts" onClick={(e) => handleClick(e, "contacts")}>
+                  <button type="button">{t("menuContact")}</button>
+                </a>
+              </li>
+            </nav>
+            <PaymentButton />
+          </div>
+
           <div className={styles.mobile}>
             <GiHamburgerMenu
               className={styles.gamburger}
               onClick={handleClickHamburger}
             />
-            <nav className={` ${isOpen ? styles.menu_mobile : styles.menu}`}>
+            <nav className={`${styles.menu_mobile} ${isOpen ? styles.open : ""}`}>
               <li>
                 <a href="#services" onClick={(e) => handleClick(e, "services")}>
                   <button type="button">{t("menuService")}</button>
@@ -88,12 +93,15 @@ const Main = () => {
           <h1 className={styles.title}>{t("mainName")}</h1>
           <p className={styles.text}>{t("mainText")}</p>
           {/* {!isPopupOpen && ( */}
-            {/* <button className={styles.button} onClick={togglePopup}>
+          {/* <button className={styles.button} onClick={togglePopup}>
               {t("getConsultation")}
             </button> */}
           {/* )} */}
           {/* <Popup isPopupOpen={isPopupOpen} togglePopup={togglePopup} /> */}
-          <WhatsAppButton className={styles.button} text={t('getConsultation')} />
+          <WhatsAppButton
+            className={styles.button}
+            text={t("getConsultation")}
+          />
         </div>
       </main>
     </>
---
+++ src/components/PaymentButton/PaymentButton.jsx
@@ -0,0 +1,13 @@
+import styles from "./PaymentButton.module.scss";
+
+const PaymentButton = () => {
+  return (
+    <>
+      <button type="button" className={styles.button}>
+        PaymentButton
+      </button>
+    </>
+  );
+};
+
+export default PaymentButton;
